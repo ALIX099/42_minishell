a8980a87ce644c1460d6822d471de9aa247c556e
Ali Bouknana <abouknan@c4r8p4.1337.ma>
2025-08-24 00:42:29 +0100

    Sun Aug 24 12:42:29 AM +01 2025

---
+++ libft/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouknan <abouknan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 18:00:19 by ikarouat          #+#    #+#             */
+/*   Updated: 2025/08/11 13:19:00 by abouknan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*new_s;
+	size_t	s_len;
+
+	if (!s1)
+		return (NULL);
+	i = 0;
+	s_len = ft_strlen(s1);
+	new_s = (char *)malloc(s_len + 1);
+	if (!new_s)
+		return (NULL);
+	while (i < s_len)
+	{
+		new_s[i] = s1[i];
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
+}
